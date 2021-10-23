5b6498b5b7060764ebf7505c048bd80c342f416b
manuel <manuel.gugger@gmail.com>
2021-10-23 18:26:56 +0200

    remove k3s

--- environments/home/terragrunt.hcl
+++ environments/home/terragrunt.hcl
@@ -12,7 +12,7 @@ inputs = {
   location = "switzerlandnorth"
   connectivity_resource_group_name = "tf-connectivity"
   storage_resource_group_name = "tf-storage"
-  k3s_resource_group_name = "tf-k3s"
+  vms_resource_group_name = "tf-vms"
   customimages_resource_group_name = "tf-vmimages"
   vm_snapshot_resource_id = "/subscriptions/9cd44e3c-5f40-4eea-95f7-bb551bac9cdd/resourceGroups/tf-vmimages/providers/Microsoft.Compute/snapshots/archlinux_gen1_snapshot"
 }
---
+++ environments/home/vm-internal.tf
@@ -0,0 +1,54 @@
+resource "azurerm_resource_group" "tf-vms" {
+  name     = var.vms_resource_group_name
+  location = var.location
+}
+
+resource "azurerm_network_interface" "nic-server1" {
+  name                = "server1-nic"
+  location            = azurerm_resource_group.tf-vms.location
+  resource_group_name = azurerm_resource_group.tf-vms.name
+  enable_ip_forwarding = true
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_managed_disk" "server1-osdisk" {
+  name = "vm-server1-osdisk"
+  os_type              = "Linux"
+  location = var.location
+  resource_group_name = azurerm_resource_group.tf-vms.name
+  storage_account_type = "Premium_LRS"
+  create_option = "Copy"
+  source_resource_id = var.vm_snapshot_resource_id
+  disk_size_gb = "16"
+
+  tags = {
+    environment = "home"
+  }
+}
+
+resource "azurerm_virtual_machine" "vm-server1" {
+  name                  = "vm-server1"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.tf-vms.name
+  vm_size               = "Standard_B1ms"
+  network_interface_ids = [
+    azurerm_network_interface.nic-server1.id,
+  ]
+
+  storage_os_disk {
+    name              = "vm-server1-osdisk"
+    os_type = "linux"
+    managed_disk_id   = "${resource.azurerm_managed_disk.server1-osdisk.id}"
+    create_option     = "Attach"
+  }
+
+  tags = {
+    environment = "home"
+    wireguard_ip = "192.168.8.5/32"
+  }
+}
